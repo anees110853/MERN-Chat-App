292a031c13fa2ffa8889e7559e31be3201c7ec65
BrainX MERN <anees.rehman@brainxtech.com>
2024-05-20 16:09:29 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect, useMemo} from 'react'
+import {io} from 'socket.io-client'
 
-function App() {
+const App = () => {
+
+  const [message, setMessage] = useState();
+  const [room, setRoom] = useState();
+  const [socketId, setSocketId] = useState();
+
+  const socket = useMemo(()=>io('http://localhost:4000'),[])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('message', {room:room, message:message})
+  }
+
+  const handleMessage = (e) =>{
+    e.preventDefault();
+    socket.emit('send',({room:room, message:message}))
+
+    setMessage(e.target.value)
+  }
+
+  const handleRoom = (e) =>{
+    e.preventDefault();
+    setRoom(e.target.value)
+  }
+
+
+  useEffect(()=>{
+
+    socket.on('connect',(data)=>{
+      setSocketId(socket.id)
+    })
+
+    socket.on('all-chat',(data)=>{
+      console.log(data)
+    })
+
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <p>{socketId}</p>
+    <input type='text' onChange={(e)=> handleMessage(e)}/>
+    <input type='text' onChange={(e)=> handleRoom(e)}/>
+    <button onClick={(e)=>handleSubmit(e)}>Submit</button>
+    </>
+  )
 }
 
-export default App;
+export default App
